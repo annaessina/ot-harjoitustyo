b2499b087904c3bad1450a074f359b457ccb6846
Anna Essina <anna.essina@helsinki.fi>
2022-03-29 20:17:33 +0300

    kassapaate

---
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -0,0 +1,9 @@
+import unittest
+from kassapaate import Kassapaate
+from maksukortti import Maksukortti
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassapaate = Kassapaate(
+            
+        )
