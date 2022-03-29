a94b5bed446d4d89a012f616bdc83546399b04d3
Anna Essina <anna.essina@helsinki.fi>
2022-03-29 18:04:15 +0300

    unicafe tehtava

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -9,8 +9,19 @@ class TestMaksukortti(unittest.TestCase):
         self.assertNotEqual(self.maksukortti, None)
 
     def test_kortin_saldo_oikein(self):
-        self.asserEqual(str(self.maksukortti), "saldo: 5.0")
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
 
     def test_rahan_lataus_toimii(self):
         self.maksukortti.lataa_rahaa(10)
-        self.asserEqual(str(self.maksukortti), "saldo: 20.0")
+        self.assertEqual(str(self.maksukortti), "saldo: 20.0")
+
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(str(self.maksukortti), "saldo: 5.0")
+
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
+
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "saldo: 3.0")
+
+        self.assertEqual(self.maksukortti.ota_rahaa(100), False)
