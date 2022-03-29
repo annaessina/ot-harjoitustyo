f4b510f60fa680c58699dcddd3fd6e4b254c1f08
Anna Essina <anna.essina@helsinki.fi>
2022-03-29 17:46:12 +0300

    koodi aloitettu

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,10 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_oikein(self):
+        self.asserEqual(str(self.maksukortti), "saldo: 5.0")
+
+    def test_rahan_lataus_toimii(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.asserEqual(str(self.maksukortti), "saldo: 20.0")
