0ec50bb93fbf1bfff3d10917803a6fc5e36f88e1
Anna Essina <anna.essina@helsinki.fi>
2022-03-29 18:15:15 +0300

    koodia muokattu

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -9,19 +9,19 @@ class TestMaksukortti(unittest.TestCase):
         self.assertNotEqual(self.maksukortti, None)
 
     def test_kortin_saldo_oikein(self):
-        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.10")
 
     def test_rahan_lataus_toimii(self):
         self.maksukortti.lataa_rahaa(10)
-        self.assertEqual(str(self.maksukortti), "saldo: 20.0")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.20")
 
     def test_rahan_ottaminen_toimii(self):
         self.maksukortti.ota_rahaa(5)
-        self.assertEqual(str(self.maksukortti), "saldo: 5.0")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.05")
 
         self.assertEqual(self.maksukortti.ota_rahaa(5), True)
 
         self.maksukortti.ota_rahaa(100)
-        self.assertEqual(str(self.maksukortti), "saldo: 3.0")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.03")
 
         self.assertEqual(self.maksukortti.ota_rahaa(100), False)
