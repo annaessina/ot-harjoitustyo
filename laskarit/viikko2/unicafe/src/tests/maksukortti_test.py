21fe16a0ee5f7f1146b93b008de6233d046ba1a2
Anna Essina <anna.essina@helsinki.fi>
2022-03-30 20:34:43 +0300

    maksukortti tehtava valmis

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -8,7 +8,25 @@ class TestMaksukortti(unittest.TestCase):
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
-    def kortin_saldo_alussa_oikein(self):
+    def test_kortin_saldo_alussa_oikein(self):
         self.assertEqual(self.maksukortti.saldo, 10)
     
+    def test_rahan_lataaminen_oikein(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(self.maksukortti.saldo, 20)
+
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(self.maksukortti.saldo, 5)
     
+    def test_rahan_maara_ei_muutu(self):
+        self.maksukortti.ota_rahaa(15)
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_raha_ei_riita(self):
+        self.maksukortti.ota_rahaa(15)
+        self.assertNotEqual(self.maksukortti.ota_rahaa, True)
+
+    def test_raha_ei_riita(self):
+        self.maksukortti.ota_rahaa(8)
+        self.assertNotEqual(self.maksukortti.ota_rahaa, False)
