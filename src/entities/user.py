9b8f8153c5699031baa29b81a8f9f0841eccbfb4
Anna Essina <anna.essina@helsinki.fi>
2022-04-26 22:19:56 +0300

    user.py tiedoston luonti

---
+++ src/entities/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, username: str, password: str, add: float, dist: float, mult: float, div: float):
+        self.username = username
+        self.password = password
+        self.add = add
+        self.dist = dist
+        self.mult = mult
+        self.div = div
